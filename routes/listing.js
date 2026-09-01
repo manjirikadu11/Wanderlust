5f948d24b353cde21883195b62f99d3d209bc1d9
Manjiri Kadu <manjirikadu9@gmail.com>
2026-09-02 00:16:33 +0530

    Continue authentication implementation

--- routes/listing.js
+++ routes/listing.js
@@ -30,6 +30,10 @@ router.get("/new", (req, res) => {
 router.get("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if(!listing) {
+        req.flash("error", "Listing you requested for does not exist!");
+        res.redirect("/listings");
+    }
     res.render("listings/show.ejs", { listing });
 }));
 
@@ -38,6 +42,7 @@ router.post("/", validateListing, wrapAsync(async (req, res, next) => {
     let result = listingSchema.validate(req.body);
     const newListing = new Listing(req.body.listing);
     await newListing.save();
+    req.flash("success", "New Listing Created!");
     res.redirect("/listings");
 }));
 
@@ -45,6 +50,10 @@ router.post("/", validateListing, wrapAsync(async (req, res, next) => {
 router.get("/:id/edit", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
+     if(!listing) {
+        req.flash("error", "Listing you requested for does not exist!");
+        res.redirect("/listings");
+    }
     res.render("listings/edit.ejs", { listing });
 }));
 
@@ -63,6 +72,7 @@ router.put("/:id", validateListing, wrapAsync(async (req, res) => {
         },
         { runValidators: true }
     );
+    req.flash("success", "Listing Updated!");
     res.redirect("/listings");
 }));
 
@@ -73,6 +83,8 @@ router.delete("/:id", wrapAsync(async (req, res) => {
     let deletedListing = await Listing.findByIdAndDelete(id);
 
     console.log(deletedListing);
+
+    req.flash("success", "Listing Deleted!");
 
     res.redirect("/listings");
 }));
